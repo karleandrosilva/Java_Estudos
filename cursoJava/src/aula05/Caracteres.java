5cd3337e80efd1cf22df1f451ea5c38bd8b544e9
Karleandro <143852351+karleandrosilva@users.noreply.github.com>
2024-05-22 22:28:15 -0300

    update aula 05 parte 1

---
+++ cursoJava/src/aula05/Caracteres.java
@@ -0,0 +1,16 @@
+package aula05;
+
+public class Caracteres {
+
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 80;
+		System.out.println(valor);
+		
+		valor = (char) (valor + 1);
+		System.out.println(valor);
+	}
+
+}
