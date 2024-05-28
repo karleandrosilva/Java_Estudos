802917ff5766ab19232ed6e02168e2ca31f692fc
Karleandro <143852351+karleandrosilva@users.noreply.github.com>
2024-05-28 18:53:57 -0300

    aula sobre while

---
+++ cursoJava/src/aula09/Pratica01.java
@@ -0,0 +1,12 @@
+package aula09;
+
+public class Pratica01 {
+	public static void main(String[] args) {
+		int contador = 0;
+		
+		while (contador <= 10) {
+			System.out.println(contador);
+			contador += 1;
+		}
+	}
+}
